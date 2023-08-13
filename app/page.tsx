9046ba53a60ba855ccadb1a3549453108d0b0ab3
Vinicius Costa <vinicius.dsc95@gmail.com>
2023-08-13 19:11:01 -0300

    fix: flex responsible cards

--- app/page.tsx
+++ app/page.tsx
@@ -61,125 +61,123 @@ export default function SpeedBreakpointPage() {
   return (
     <section className="container mx-auto flex flex-col gap-8 p-4">
       <H2>Speed breakpoints</H2>
-      <div className="flex flex-col gap-8">
-        <div className="flex gap-8 align-middle">
-          <Card>
-            <CardHeader>
-              <CardTitle>Character Info</CardTitle>
-              <CardDescription>
-                Enter your character information
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <Form {...form}>
-                <form>
-                  <div className="flex flex-wrap gap-4">
-                    <FormField
-                      control={form.control}
-                      name="level"
-                      render={({ field }) => (
-                        <FormItem className="flex-grow basis-11">
-                          <FormLabel className="block">Level</FormLabel>
-                          <FormControl>
-                            <Input
-                              {...field}
-                              type="number"
-                              placeholder="Enter your character's level"
-                              onChange={(e) => {
-                                field.onChange(e.target.value)
-                                setItemFromLocalStorage('level', e.target.value)
-                              }}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="spell"
-                      render={({ field }) => (
-                        <FormItem className="flex-grow">
-                          <FormLabel className="block">Haste spell</FormLabel>
-                          <Select
-                            value={field.value}
-                            onValueChange={(value) => {
-                              field.onChange(value)
-                              setItemFromLocalStorage('spell', value)
+      <div className="flex flex-wrap justify-start gap-8">
+        <Card className="flex-1">
+          <CardHeader>
+            <CardTitle>Character Info</CardTitle>
+            <CardDescription>Enter your character information</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form>
+                <div className="flex flex-wrap gap-4">
+                  <FormField
+                    control={form.control}
+                    name="level"
+                    render={({ field }) => (
+                      <FormItem className="flex-grow basis-11">
+                        <FormLabel className="block">Level</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            type="number"
+                            placeholder="Enter your character's level"
+                            onChange={(e) => {
+                              field.onChange(e.target.value)
+                              setItemFromLocalStorage('level', e.target.value)
                             }}
-                            defaultValue={field.value}
-                          >
-                            <FormControl>
-                              <SelectTrigger>
-                                <SelectValue placeholder="Select a haste spell" />
-                              </SelectTrigger>
-                            </FormControl>
-                            <SelectContent>
-                              <SelectItem value="none">none</SelectItem>
-                              <SelectSeparator />
-                              <SelectItem value="haste">utani hur</SelectItem>
-                              <SelectItem value="strongHaste">
-                                utani gran hur
-                              </SelectItem>
-                              <SelectItem value="charge">
-                                utani tempo hur
-                              </SelectItem>
-                              <SelectItem value="swiftFoot">
-                                utamo tempo san
-                              </SelectItem>
-                            </SelectContent>
-                          </Select>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                </form>
-              </Form>
-            </CardContent>
-          </Card>
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="spell"
+                    render={({ field }) => (
+                      <FormItem className="flex-grow">
+                        <FormLabel className="block">Haste spell</FormLabel>
+                        <Select
+                          value={field.value}
+                          onValueChange={(value) => {
+                            field.onChange(value)
+                            setItemFromLocalStorage('spell', value)
+                          }}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Select a haste spell" />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value="none">none</SelectItem>
+                            <SelectSeparator />
+                            <SelectItem value="haste">utani hur</SelectItem>
+                            <SelectItem value="strongHaste">
+                              utani gran hur
+                            </SelectItem>
+                            <SelectItem value="charge">
+                              utani tempo hur
+                            </SelectItem>
+                            <SelectItem value="swiftFoot">
+                              utamo tempo san
+                            </SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
 
-          <Card>
-            <CardHeader>
-              <CardTitle>Character status</CardTitle>
-              <CardDescription>
-                This is your detailed character speed
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead className="w-8">base</TableHead>
-                    <TableHead className="w-8">modifiers</TableHead>
-                    <TableHead className="w-8">total</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  <TableRow>
-                    <TableCell>{charSpeed.base}</TableCell>
-                    <TableCell>{charSpeed.modifiers}</TableCell>
-                    <TableCell>{charSpeed.totalWithModifiers}</TableCell>
-                  </TableRow>
-                </TableBody>
-              </Table>
-            </CardContent>
-          </Card>
-        </div>
-
-        <DataTable columns={columns} data={rowsData} speed={charSpeed}>
-          <DataTable.Caption className="mt-0 bg-zinc-100 py-4">
-            <div className="flex items-center justify-center gap-8">
-              {caption.map(({ id, label, skin }) => (
-                <span key={id} className="flex items-center gap-2">
-                  <CaptionIndicator skin={skin} />
-                  <span>{label}</span>
-                </span>
-              ))}
-            </div>
-          </DataTable.Caption>
-        </DataTable>
+        <Card className="flex-grow">
+          <CardHeader>
+            <CardTitle>Character status</CardTitle>
+            <CardDescription>
+              This is your detailed character speed
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Table className="overflow-hidden rounded-md">
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-8 text-center">base</TableHead>
+                  <TableHead className="w-8 text-center">modifiers</TableHead>
+                  <TableHead className="w-8 text-center">total</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell skin="neutral">{charSpeed.base}</TableCell>
+                  <TableCell skin="neutral">{charSpeed.modifiers}</TableCell>
+                  <TableCell skin="neutral">
+                    {charSpeed.totalWithModifiers}
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
       </div>
+
+      <DataTable columns={columns} data={rowsData} speed={charSpeed}>
+        <DataTable.Caption className="mt-0 bg-zinc-100 py-4">
+          <div className="flex items-center justify-center gap-8">
+            {caption.map(({ id, label, skin }) => (
+              <span key={id} className="flex items-center gap-2">
+                <CaptionIndicator skin={skin} />
+                <span>{label}</span>
+              </span>
+            ))}
+          </div>
+        </DataTable.Caption>
+      </DataTable>
     </section>
   )
 }
